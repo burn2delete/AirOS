ed1629133020003a088bf1cc0dcb9e373bc19a93
Matthew Ratzke <matthew.003@me.com>
2012-08-11 03:06:02 -0600

    revert AppKernel changes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new airos\corebundle\AirOSCoreBundle()
+            new AirOS\CoreBundle\AirOSCoreBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
